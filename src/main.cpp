0c039dd703e3ebcab89825f55d26a47e3e1e342d
Mathias Bögel <mathias.bogel@insa-cvl.fr>
2020-06-22 16:51:28 +0200

    Created task that connects to wifi using Arduino Framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,31 @@
-#include <Arduino.h>
-#include <Arduino_FreeRTOS.h>
+#include <WiFi.h>
+void wifiConnection(void *pvParameters);
+
+const char* ssid = "networkESP32";
+const char* pass = "azerty1234";
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  xTaskCreate(wifiConnection, "wifi connection", 4096, NULL, 1,NULL);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(2000);
+}
+
+void wifiConnection(void *pvParameters) {
+
+  Serial.print("Connection to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED ) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+  Serial.print("IP address is ");
+  Serial.println(WiFi.localIP());
+  for ( ;; ) {
+    delay(1000);
+  }
 }
